ed8d8cea97111514d1c1f3ff32afd36d82ea11ce
Cap LaBuff <lucasbeef00@gmail.com>
2026-07-23 08:26:40 +0200

    fix version

--- LaLaDancer/Plugin.cs
+++ LaLaDancer/Plugin.cs
@@ -4,7 +4,7 @@ using RiftOfTheNecroManager;
 namespace LaLaDancer;
 
 
-[BepInPlugin("com.lalabuff.necrodancer.laladancer", "LaLaDancer", "0.0.1")]
+[BepInPlugin("com.lalabuff.necrodancer.laladancer", "LaLaDancer", "0.1.0")]
 [NecroManagerInfo(menuNameOverride: "LaLaDancer")]
 public class Plugin : RiftPlugin {
 }
