be77f5b0dad898a49e1b7b36b678987d757d5356
Cap LaBuff <lucasbeef00@gmail.com>
2026-07-25 03:22:12 +0200

    update config descriptions

--- LaLaDancer/Config.cs
+++ LaLaDancer/Config.cs
@@ -7,9 +7,9 @@ public static class Config {
     public static class Bugfixes {
         const string GROUP = "Bugfixes";
         
-        public static Setting<bool> PredictiveSfx { get; } = new(GROUP, "Fix Trap Hitsounds", true, "Fixes the timing of hitsounds for enemies which are affected by traps.");
+        public static Setting<bool> PredictiveSfx { get; } = new(GROUP, "Fix Trap Hitsounds", true, "Fixes the timing of hitsounds for enemies which are affected by traps near the action row.");
         public static Setting<bool> ArmadilloSfx { get; } = new(GROUP, "Fix Armadillo Hitsounds", true, "Fixes the timing of hitsounds for armadillos on subdivisions not divisible by 3.");
-        public static Setting<bool> BlademasterSfx { get; } = new(GROUP, "Fix Blademaster Sounds", true, "Fixes the speed of blademaster sound effects.");
+        public static Setting<bool> BlademasterSfx { get; } = new(GROUP, "Fix Blademaster Sounds", true, "Fixes the speed of blademaster sound effects on charts with BPM changes.");
         public static Setting<bool> ScoreDisplay { get; } = new(GROUP, "Fix Score Display", true, "Adds more digits to the in-game score display for scores above 10 million.");
         public static Setting<bool> CustomParticles { get; } = new(GROUP, "Fix Custom Particles", true, "Fixes a bug which causes only the first 75% of custom particles to be displayed in custom charts.");
         public static Setting<bool> Countdown { get; } = new(GROUP, "Fix Countdown Speed", true, "Fixes the countdown speed when using practice mode in a chart with BPM changes.");
